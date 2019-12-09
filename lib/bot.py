c507317b9bbcf863c8261b769df6b57dc0955e21
Markus Dutschke <markus.dutschke@auxolutions.de>
2019-12-09 09:23:22 +0100

    adding randCat to get rid of anti-learning

--- lib/bot.py
+++ lib/bot.py
@@ -1923,7 +1923,7 @@ class PlayerAI_full_v0(AbstractPlayer):
         """
         self.rrm = self.rrm[-self.lenRrReplayMem:]
     
-    def train(self, nGames, pRandCat=0, pRatCat=None,
+    def train(self, nGames, pRandCat=0.1, pRatCat=None,
               pOptRr=.3, pRandRr=0.1, pRatRr=10):
         """Training the Player with nGames and based on the trainers moves.
     
