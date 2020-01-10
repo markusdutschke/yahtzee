0cc4ad3f100435c67c9b293dc7d6630b3fd8d289
Markus Dutschke <markus.dutschke@auxolutions.de>
2020-01-10 18:26:37 +0100

    open branch

--- test.py
+++ test.py
@@ -6,17 +6,38 @@ Created on Fri Nov 22 18:41:30 2019
 @author: user
 """
 
+#import numpy as np
+#
+#def split_in_consec_ints(lst):
+#    splits = [0]
+#    for ii, el in enumerate(lst[:-1]):
+#        if el + 1 < lst[ii+1]:
+#            splits += [ii+1]
+#    splits += [len(lst)]
+#    print(splits)
+#    slst = [lst[a:b] for a,b in zip(splits, splits[1:])]
+#    return slst
+#
+#print(split_in_consec_ints([1,3,4, 5 , 7,8, 10]))
+#print(sum([1, 2], 0))
+
 import numpy as np
 
-def split_in_consec_ints(lst):
-    splits = [0]
-    for ii, el in enumerate(lst[:-1]):
-        if el + 1 < lst[ii+1]:
-            splits += [ii+1]
-    splits += [len(lst)]
-    print(splits)
-    slst = [lst[a:b] for a,b in zip(splits, splits[1:])]
-    return slst
+myInt = 5
+myList = [1,2,3]
 
-print(split_in_consec_ints([1,3,4, 5 , 7,8, 10]))
-print(sum([1, 2], 0))
+def process(varInt, varList):
+    varInt = 7
+    tmpList = list(varList)
+#    print(32, tmpList)
+#    tmpList = varList
+    tmpList[0] = 9
+#    varList[0] = 9
+#    varList = [99, 98]
+    return None
+
+print(myInt, myList)
+
+process(myInt, myList)
+
+print(myInt, myList)
